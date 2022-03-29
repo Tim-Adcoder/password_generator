54ce742892f050b2e705bd6046d5654ffe0f2604
Tim Siu <anson242004@hotmail.com>
2022-03-30 08:50:46 +1030

    reorganise and debug js file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code 
 var generateBtn = document.querySelector("#generate");
-var charLength = 8;
+var characterLength = 8;
 var chosenAry = [];
 
 var specialCharAry = ['`','~','!','@','#','$','%','^','&','*','(',')','-','_','+','=','|','?','/','<','>',';',':'];
@@ -8,68 +8,60 @@ var lowercaseAry = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
 var uppercaseAry = ['A','B','C','D','E','F','G','H','I','J','K','L','M','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var numberAry = ['1','2','3','4','5','6','7','8','9','0'];
 
-//creat a "generatePassword" function
-function generatePassword() {
-  console.log("Good work!");
-
-
-// prompt the user 
-// 1. password length 8 < 128
-// 2. choose to have lowercase,
-// 3. uppercase
-// 4. numeric
-// 5. special characters
-
-// validate input 
-// display password based on ceriteria
-  return "Generated password will be shown here.";
-}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
   var prompts = getPrompt()
+  var passwordText = document.querySelector("#password");
 
-  if (Prompts) {
+
+  if (prompts) {
     var password = generatePassword();
-    var passwordText = document.querySelector("#password");
     passwordText.value = password;
+  } else {
+    passwordTest.value = "";
   }
-
 }
 
-function generatePassword() {
-  var finalPassword = "";
-for(var i = 0; i < charLength; i++) {
-  var randomoIndex = Math.floor(random() * chosenAry.length);
-  password = password + choiceAry[randomIndex];
-}
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
+// choose prompts 
 function getPrompt() {
-  choiceAry = [] 
+  chosenAry = [];
 
   characterLength = prompt("Between 8-128, How many characters do you want for the password?")
 
-  if(isNaN(characterLength) || characterLength < 8 || charLength > 128) {
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    // false alart
     alert("Please choose character length between 8 - 128. Let's try again. " );
     return false; 
   } 
 
   if(confirm("Lowercase letters in the password?")) {
-    chosenAry = choiceAry.concat(lowercaseAry);
+    chosenAry = chosenAry.concat(lowercaseAry);
   }
   if(confirm("Uppercase letters in the password?")) {
-    chosenAry = choiceAry.concat(uppercaseAry);
+    chosenAry = chosenAry.concat(uppercaseAry);
   }
   if(confirm("spcial characters in the password?")) {
-    chosenAry = choiceAry.concat(specialCharAry);
+    chosenAry = chosenAry.concat(specialCharAry);
   }
   if(confirm("How about numbers?")) {
-    chosenAry = choiceAry.concat(numberAry);
+    chosenAry = chosenAry.concat(numberAry);
   }
   return true;
+  
 }  
+
+function generatePassword() {
+  var finalPassword = "";
+for(var i = 0; i < characterLength; i++) {
+    var randomIndex = Math.floor(Math.random() * chosenAry.length);
+    finalPassword = finalPassword + chosenAry[randomIndex];
+}
+return finalPassword;
+
+}
+
+
+
